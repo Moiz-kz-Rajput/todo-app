24896277b313a6419a21e0d5b1f036f3e676b328
Moiz-kz-Rajput <moizkhanzada@gmail.com>
2013-07-30 20:14:47 +0500

    data inser but not view

---
+++ public/javascripts/script.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+var os=require('os');
+
+var message='Hello this scrip is running on node-dev.js '+process.version+' on a '+os.type()+' -based operating system.';
+
+console.log(message);
